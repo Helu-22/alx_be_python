1935db6b5c03313a4690579bcbe37becb7aa09db
Helu-22 <helen.yitbarekk22@gmail.com>
2025-05-26 21:50:19 +0300

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -3,4 +3,4 @@ number2 = 5
 sum = number1 + number2
 difference = number2 - number1
 product = number1 * number2
-print = [operation] of [number1] and [number2] is [sum].
+print = [operation] of (number1) and (number) is (sum).
