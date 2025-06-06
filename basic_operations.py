132917e79207de872c27353e296bf5b4870fd2e6
Helu-22 <helen.yitbarekk22@gmail.com>
2025-06-06 21:25:21 +0300

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -1,6 +1,13 @@
+# Assign values to variables
 number1 = 10
 number2 = 5
-sum = number1 + number2
-difference = number2 - number1
-product = number1 * number2
-print = [operation] of (number1) and (number) is (sum).
+
+# Perform arithmetic operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+# Print the results in the required format
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
